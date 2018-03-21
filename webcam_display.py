53f593e6c6558a4afc4d7e5767c50b60c4d042f9
Shane <magnetscoil@gmail.com>
2018-03-21 14:17:04 -0700

    websocket cam

--- webcam_display.py
+++ webcam_display.py
@@ -16,16 +16,30 @@ import sys
 def signal_handler(signal, frame):
         colorWipe(strip, Color(0,0,0))
         sys.exit(0)
-
-# def opt_parse():
-#         parser = argparse.ArgumentParser()
-#         parser.add_argument('-c', action='store_true', help='clear the display on exit')
-#         args = parser.parse_args()
-#         if args.c:
-#                 signal.signal(signal.SIGINT, signal_handler)
+host = 'localhost'
+port = 5050
+isock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+isock.bind((host, port))
+try:
+   isock.listen(1)
+   conn, addr = isock.accept()
+   recv_val = []
+   while True:
+       dim = conn.recv(1024)
+       if not dim:
+           break
+       else:
+           recv_val.append(dim)
+except:
+    print("Could not establish connection: ")
+recv_val = pickle.loads(recv_val[0])
+print(recv_val)
+w = recv_val[0] # width of pixel matrix
+h = recv_val[1] # height of pixel matrix
+img_rgb_matrix = [[[] for x in range(h)] for y in range(w)] # construct matrix to hold rgb vals
 
 # LED strip configuration:
-LED_COUNT      = 192    # Number of LED pixels.
+LED_COUNT      = w*h      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
@@ -35,56 +49,41 @@ LED_INVERT     = False   # True to invert the signal (when using NPN transistor 
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 #LED_STRIP      = ws.WS2811_STRIP_RGB   # Strip type and colour ordering
 
-host = 'localhost'
-port = 5050
-isock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-isock.bind((host, port))
-try:
-   isock.listen(1)
-   conn, addr = isock.accept()
-   message = []
-   while True:
-       dim = conn.recv(1024)
-       if not dim:
-           break
-       else:
-           message.append(dim)
-except:
-    print("Could not establish connection: ")
-message = pickle.loads(message[0])
-print(message)
-w = 12 # width of pixel matrix
-h = 16   # height of pixel matrix
-img_rgb_matrix = [[[] for x in range(h)] for y in range(w)] # construct matrix to hold rgb vals
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((host, port))
+def get_connection():
+    try:
+       sock.listen(1)
+       conn, addr = sock.accept()
+       recv_vals = []
+       while True:
+           dim = conn.recv(1024)
+           if not dim:
+               break
+           else:
+               recv_vals.append(dim)
+    except:
+        print("Could not establish connection: ")
+    return(pickle.loads(recv_vals[0]))
 
-
-def display_img(strip, matrix):
-
+def display_img(strip):
+    print("Diplaying image")
     while True:
-        ret, frame = cap.read()
-        frame = cv2.resize(frame, (5,5), cv2.INTER_CUBIC)
+        vals = get_connection()
         
         for i in range(h):
             for j in range(w):
-                led_index = (w*h)- 1 - int(i*w+j)
-                color = frame[i,j]
-#                strip.setPixelColor(led_index, Color(*color))
-#    strip.show()
-
-def sample_image(img):
-    for i in range(h):                               #iterate through rows
-            for j in range(w):                           # iterate through columns
-                img_x = j
-                if i%2 == 0:
-                    img_x = (w-1)-j
-                img_rgb_matrix[j][i] = img[img_x,i]   # load matrix with rgb values
-    return(img_rgb_matrix)
+                led_index = w*h-1
+                color = vals[i,j]
+                strip.setPixelColor(led_index, Color(*color))
+        strip.show()
 
 # Main program logic follows:
 if __name__ == '__main__':
     # Create NeoPixel object with appropriate configuration.
-#    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
     # Intialize the library (must be called once before other functions).
-#    strip.begin()
+    strip.begin()
     print("at least it ran")
-    # display_img(strip, img_rgb_matrix)#print(array(img)))
+    display_img(strip)
+    #print(array(img)))
